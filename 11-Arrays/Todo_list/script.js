579afb57c02972e74311eb5ce8f621dd3a60268c
Sajid <sajidahamed2001@gmail.com>
2024-10-17 19:33:47 +0530

    complete

--- 11-Arrays/Todo_list/script.js
+++ 11-Arrays/Todo_list/script.js
@@ -1,10 +1,36 @@
-listarray = []
-function addtodo() {
-    //console.log('sdsd');
-    let listitem = document.querySelector('.input').value
-    listarray.push(listitem)
-    console.log(listitem);
-    console.log(listarray);
+array = [
 
+]
+
+function addtodo(){
+    let task = document.querySelector('.input').value
+    let date = document.querySelector('.date').value
+    array.push({task,date})
+    renderHTML()
+    console.log(array);    
+}
+function renderHTML(){
+    let inner_HTML =''
+    for (let index = 0; index < array.length; index++) {
+        const arrayObject = array[index]
+        console.log(arrayObject);
+        const task = arrayObject.task
+        const date = arrayObject.date
+
+        const htmlString = `<div class = "lineitem">
+        <div>${task}</div>
+        <div>${date}</div>
+        <button class = "delete-button" onclick = deleteTodo(${index})>Delete</button> 
+        </div>
+        `;
+
+        inner_HTML = inner_HTML + htmlString
+
+    }
+    document.querySelector('.listtasks').innerHTML = inner_HTML
 }
 
+function deleteTodo(index) {
+    array.splice(index, 1); // Remove the item from the array
+    renderHTML(); // Re-render the tasks
+}
